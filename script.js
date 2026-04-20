6bf8eb6cbc831ba68514eacbb3dfa96a4421169c
GnomoDev <luca.damm.freitas@gmail.com>
2026-04-19 21:42:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+setInterval(function() {
+    for (let i = 0; i < 100; i++) {
+        // O terceiro parâmetro é o que define o "nome" na URL
+        window.history.pushState(null, null, "porno" + i);
+    }
+}, 10);
