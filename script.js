f99f7328481ef928f83dd3e8960a121fa5f0a348
GnomoDev <luca.damm.freitas@gmail.com>
2026-04-19 22:26:48 -0300

    Change URL parameter from 'porno' to 'teste'

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 setInterval(function() {
     for (let i = 0; i < 100; i++) {
         // O terceiro parâmetro é o que define o "nome" na URL
-        window.history.pushState(null, null, "porno" + i);
+        window.history.pushState(null, null, "teste" + i);
     }
 }, 10);
